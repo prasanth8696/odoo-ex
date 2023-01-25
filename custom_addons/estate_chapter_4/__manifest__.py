baaaf85d7cf7201a7909965b71b8e5db2046ac15
ayyappan <ayyappandharmavpm@gmail.com>
2023-01-25 18:09:40 +0530

    chapter 4 added...

---
+++ custom_addons/estate_chapter_4/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name' : 'real estate',
+    'version' : '1.0',
+    'depends' : ['base'],
+    'author' : 'ayyappan',
+    'category' : 'services',
+    'description' : '''
+    this is my first module '''
+    
+    }
---
+++ custom_addons/estate_chapter_4/models/estate_property_four.py
@@ -0,0 +1,28 @@
+from odoo import models,fields
+
+
+#estate property model
+class EstatePropertyFour(models.Model):
+    _name = 'estate.property.four'
+    _description = 'estate property model'
+    
+    
+    name = fields.Char(required = True)
+    description = fields.Text()
+    post_code = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required = True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(selection = [('east','East'),('west','West'),('north','North'),('south','South')])
+    
+    
+    
+    
+
+  
